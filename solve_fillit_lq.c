b1c1c78a6469ad516732c8bea50bb08a6b9ee774
Lexi <lexiqt@gmail.com>
2017-08-25 00:11:11 -0400

    some thoughts ont the solve function

---
+++ solve_fillit_lq.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solve_fillit_lq.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aquint <aquint@42.us.org>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/24 00:16:17 by aquint            #+#    #+#             */
+/*   Updated: 2017/08/24 00:16:41 by aquint           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	r_fillit_solver(char **board, t_piece *head)
+{
+	char **piece = head->content;
+
+}
+
+
+char	**fillit_board(int size)
+{
+
+}
+
+int	fillit_solver(char **array, int row, int col, char **array)	
+{
+
+}
+
+int	solve_print(t_piece *head)
+{
+	int 	bsize;
+	char 	**board	
+
+	bsize = head->number;
+	board = fillit_board(bsize);
+	while (
+}
